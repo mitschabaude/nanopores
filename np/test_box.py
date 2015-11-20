873a1560df96e41b155b49ea154a0d4829116464
gregor <gregor.mitscha-eibl@gmx.at>
2015-11-20 17:38:46 +0100

    test_box.py

--- np/test_box.py
+++ np/test_box.py
@@ -3,17 +3,6 @@ from nanopores import *
 A = Box([0,0,0], [2,2,2])
 B = Box([1,1,0], [3,3,1])
 C = Box([0.5,-0.5,0], [4,0.5,1])
-A_ = Box([0,1e-12], [2,2])
-
-#print A == A_ # True
-#print A < B # True
-
-#AB = Box.union(A, B)
-#print AB
-#AC = Box.union(A, C)
-#print AC
 
 ABC = Box.union(A, B, C)
-
-#print ABC
 ABC.plot()
