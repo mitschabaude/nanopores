cd7c2143fdd8e9b4b6223fdd1332cf5f84d6173b
Benjamin <e1226175@student.tuwien.ac.at>
2015-11-25 18:45:57 +0100

    fixed indicator functions

--- random_walk_aHem/run.py
+++ random_walk_aHem/run.py
@@ -1,14 +1,8 @@
-import random,math
+import random
 from math import sqrt, pi
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
-import pylab
 from nanopores import *
-from nanopores.physics.exittime import ExitTimeProblem
 from dolfin import *
-import sys
 
 def radius(x,y):
     return sqrt(x**2+y**2)
@@ -16,17 +10,22 @@ def radius(x,y):
 def F(x,y,z):
     return [0,0,-5e-3]
 
+def argument(x,y,z):
+    return np.array([float(x),float(y),float(z)])
+
 geo = geo_from_xml("aHem")
-chi_fluid = geo.indicator("fluid_bulk_top",callable=True)
+indicator_ahem = geo.indicator("ahem",callable=True)
+indicator_molecule = geo.indicator("molecule",callable=True)
+indicator_poretop = geo.indicator("poretop",callable=True)
+indicator_membrane_geo = geo.indicator("membrane",callable=True)
+def indicator_membrane(vec): #"infinite" large membrane
+    x, y, z = vec[0], vec[1], vec[2]
+    if radius(x,y)>=60.:
+        return indicator_membrane_geo(argument(10,0,z))
+    else:
+        return indicator_membrane_geo(vec)
 
-# "flui0_bulk_top"
-# "fluid_bulk_bottom"
-# "porebottom"
-# "porecenter"
-# "poretop"
-# "pore"
-
-
+counter = np.array([0,0,0,0,0]) # ahem, molecule, poretop, membrane, bulk
 
 kb=1.3806488e-23 #boltzmann [J/K]
 T= 293 #temp [K]
@@ -43,14 +42,14 @@ steps=1e3 #
 
 
 C=1/(gamma)*tau
-coeff=math.sqrt(2*D*1e9*tau)
+coeff=sqrt(2*D*1e9*tau)
 
 X=np.zeros((steps))
 Y=np.zeros((steps))
 Z=np.zeros((steps))
 
 Z[0] = 10
-X[0] = 0
+X[0] = 10
 
 time=0
 i=0
@@ -68,36 +67,48 @@ while i<=steps-2:
     X[i+1]=X[i] + coeff*xi_x + C*F(X[i],Y[i],Z[i])[0]
     Y[i+1]=Y[i] + coeff*xi_y + C*F(X[i],Y[i],Z[i])[1]
     Z[i+1]=Z[i] + coeff*xi_z + C*F(X[i],Y[i],Z[i])[2]
-    if chi_fluid(np.array([float(X[i+1]),float(Y[i+1]),float(Z[i+1])]))==0:
+    if indicator_membrane(argument(X[i+1],Y[i+1],Z[i+1]))==1:
     	exit_x = X[i+1]
     	exit_y = Y[i+1]
     	exit_z = Z[i+1]
+        counter[3] += 1
     	i+=1
     	break
+    elif indicator_ahem(argument(X[i+1],Y[i+1],Z[i+1]))==1:
+        exit_x = X[i+1]
+        exit_y = Y[i+1]
+        exit_z = Z[i+1]
+        counter[0] += 1
+        i+=1
+        break
+    elif indicator_poretop(argument(X[i+1],Y[i+1],Z[i+1]))==1:
+        exit_x = X[i+1]
+        exit_y = Y[i+1]
+        exit_z = Z[i+1]
+        counter[2] += 1
+        i+=1
+        break
+    elif indicator_molecule(argument(X[i+1],Y[i+1],Z[i+1]))==1:
+        exit_x = X[i+1]
+        exit_y = Y[i+1]
+        exit_z = Z[i+1]
+        counter[1] += 1
+        i+=1
+        break
     i+=1
-# print 'end'
+if i>steps-2:
+    counter[4] += 1
+print counter
 
 
-# X=X[:i]
-# Y=Y[:i]
-# Z=Z[:i]
+X=X[:i]
+Y=Y[:i]
+Z=Z[:i]
 
-# np.save('X',X)
-# np.save('Y',Y)
-# np.save('Z',Z)
-if exit_x != None:
-	file=open("exit_x.txt","a")
-	file.write("%f\n" %exit_x)
-	file.close()
-if exit_y != None:
-	file=open("exit_y.txt","a")
-	file.write("%f\n" %exit_y)
-	file.close()
-if exit_z != None:
-	file=open("exit_z.txt","a")
-	file.write("%f\n" %exit_z)
-	file.close()
+np.save('X',X)
+np.save('Y',Y)
+np.save('Z',Z)
 
-# print 'number of steps:',i
+print 'number of steps:',i
 # print 'time [microsec]=',time*1e-3
-# import plot
+import plot
