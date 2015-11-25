8486926739f46e4880ee7484cd786b1414be8584
Benjamin <e1226175@student.tuwien.ac.at>
2015-11-25 19:34:40 +0100

    finished 'statistic' - TODO: implement force

--- random_walk_aHem/plot.py
+++ random_walk_aHem/plot.py
@@ -1,20 +1,33 @@
-import random,math
-from math import sqrt, pi
+from math import pi
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import pylab
-    
-fig = plt.figure()
 
+fig = plt.figure()
 ax=Axes3D(fig)
 
-X=np.load('X.npy')
-Y=np.load('Y.npy')
-Z=np.load('Z.npy')
-ax.plot(X,Y,Z,c='green')
+EXIT_X=np.load('exit_x.npy')
+EXIT_Y=np.load('exit_y.npy')
+EXIT_Z=np.load('exit_z.npy')
+counter=np.load('counter.npy')
+hits = float(np.sum(counter))
+print
+print 'PROBABILITY REACHING:'
+print 
+print 'PAC-MAN: ', float(counter[1])/hits
+print
+print 'MEMBRANE: ', float(counter[3])/hits
+print
+print 'A-HEMOLYSIN: ', float(counter[0])/hits
+print
+print 'EXIT: ', float(counter[2])/hits
+print
+print 'INFINITY: ', float(counter[4])/hits
+print
+
 show_half=False
 fac=2
 if show_half:
@@ -119,7 +132,7 @@ X_aHem = np.array([[2.16, 0.0, 0.0],
 
 x_aHem, z_aHem = np.zeros_like(X_aHem), np.zeros_like(X_aHem)
 for index in range(x_aHem.shape[0]):
-  x_aHem[index], z_aHem[index] = X_aHem[index][0], X_aHem[index][2]
+    x_aHem[index], z_aHem[index] = X_aHem[index][0], X_aHem[index][2]
 x_aHem = np.append(x_aHem,x_aHem[0])
 z_aHem = np.append(z_aHem,z_aHem[0])
 angle = np.linspace(0,fac*pi,50)
@@ -132,27 +145,32 @@ Z_plot = mesh_z
 ax.plot_wireframe(X_plot, Y_plot, Z_plot,color='blue', rstride = 1, cstride = 1, alpha=1.0)
 
 if show_half:
-  x=x_aHem.tolist()
-  left=-x_aHem
-  x_=left.tolist()
-  y=np.zeros_like(x_aHem).tolist()
-  z=z_aHem.tolist()
-  verts1=[zip(x,y,z)]
-  verts2=[zip(x_,y,z)]
-  ax.add_collection3d(Poly3DCollection(verts1,color='blue',alpha=1.0))
-  ax.add_collection3d(Poly3DCollection(verts2,color='blue',alpha=1.0))
+    x=x_aHem.tolist()
+    left=-x_aHem
+    x_=left.tolist()
+    y=np.zeros_like(x_aHem).tolist()
+    z=z_aHem.tolist()
+    verts1=[zip(x,y,z)]
+    verts2=[zip(x_,y,z)]
+    ax.add_collection3d(Poly3DCollection(verts1,color='blue',alpha=1.0))
+    ax.add_collection3d(Poly3DCollection(verts2,color='blue',alpha=1.0))
 ax.set_xlim3d(-10,10)
 ax.set_ylim3d(-10,10)
 ax.set_zlim3d(-10,10)
 # Sphere
 u = np.linspace(0, 2 * np.pi, 100)
 v = np.linspace(0, np.pi, 100)
-x = 0.1*np.outer(np.cos(u), np.sin(v))
-y = 0.1*np.outer(np.sin(u), np.sin(v))
-z = 0.1*np.outer(np.ones(np.size(u)), np.cos(v))
+
+# for index in range(EXIT_X.shape[0]-1):
+#     x = 0.1*np.outer(np.cos(u), np.sin(v))
+#     y = 0.1*np.outer(np.sin(u), np.sin(v))
+#     z = 0.1*np.outer(np.ones(np.size(u)), np.cos(v))
+#     ax.plot_surface(x+EXIT_X[index], y+EXIT_Y[index], z+EXIT_Z[index],  rstride=12, cstride=5, color='r',alpha=1.0)
+ax.scatter(EXIT_X,EXIT_Y,EXIT_Z,color='red')
+
+
 x1 = 2.2*np.outer(np.cos(u), np.sin(v))
 y1 = 2.2*np.outer(np.sin(u), np.sin(v))
 z1 = 2.2*np.outer(np.ones(np.size(u)), np.cos(v))
-ax.plot_surface(x+X[X.shape[0]-1], y+Y[Y.shape[0]-1], z+Z[Z.shape[0]-1],  rstride=12, cstride=5, color='r',alpha=1.0)
 ax.plot_wireframe(x1+5., y1+0., z1+10., rstride=12, cstride=5, color='blue',alpha=1.0)
 plt.show()
--- random_walk_aHem/run.py
+++ random_walk_aHem/run.py
@@ -1,4 +1,4 @@
-import random
+from random import gauss
 from math import sqrt, pi
 import numpy as np
 from nanopores import *
@@ -25,90 +25,84 @@ def indicator_membrane(vec): #"infinite" large membrane
     else:
         return indicator_membrane_geo(vec)
 
-counter = np.array([0,0,0,0,0]) # ahem, molecule, poretop, membrane, bulk
 
 kb=1.3806488e-23 #boltzmann [J/K]
 T= 293 #temp [K]
 visc=1e-3 #[Pa*s]
 damp = 1 # diffusion should be much lower in pore than in bulk
-
 D=(kb*T)/(6*pi*visc)*damp*1e9  #diffusion[m^2/s]
-
-
 gamma=(6*pi*visc) #friction [kg/s]
-
 tau=0.1 #-1 # [ns]
 steps=1e3 # 
-
-
 C=1/(gamma)*tau
 coeff=sqrt(2*D*1e9*tau)
 
-X=np.zeros((steps))
-Y=np.zeros((steps))
-Z=np.zeros((steps))
 
-Z[0] = 10
-X[0] = 10
+counter = np.array([0,0,0,0,0]) # ahem, molecule, poretop, membrane, bulk
+EXIT_X, EXIT_Y, EXIT_Z = np.array([]), np.array([]), np.array([])
 
-time=0
-i=0
-j=0
-S=np.zeros(6)
-exit_x = None
-exit_y = None
-exit_z = None
-while i<=steps-2:
-    time+=1
-    xi_x=random.gauss(0,1)
-    xi_y=random.gauss(0,1)
-    xi_z=random.gauss(0,1)
-    
-    X[i+1]=X[i] + coeff*xi_x + C*F(X[i],Y[i],Z[i])[0]
-    Y[i+1]=Y[i] + coeff*xi_y + C*F(X[i],Y[i],Z[i])[1]
-    Z[i+1]=Z[i] + coeff*xi_z + C*F(X[i],Y[i],Z[i])[2]
-    if indicator_membrane(argument(X[i+1],Y[i+1],Z[i+1]))==1:
-    	exit_x = X[i+1]
-    	exit_y = Y[i+1]
-    	exit_z = Z[i+1]
-        counter[3] += 1
-    	i+=1
-    	break
-    elif indicator_ahem(argument(X[i+1],Y[i+1],Z[i+1]))==1:
-        exit_x = X[i+1]
-        exit_y = Y[i+1]
-        exit_z = Z[i+1]
-        counter[0] += 1
+for index in range(1000):
+    print index
+    X=np.zeros((steps))
+    Y=np.zeros((steps))
+    Z=np.zeros((steps))
+
+    Z[0] = 10
+    i=0
+    while i<=steps-2:
+        xi_x=gauss(0,1)
+        xi_y=gauss(0,1)
+        xi_z=gauss(0,1)
+        
+        X[i+1]=X[i] + coeff*xi_x + C*F(X[i],Y[i],Z[i])[0]
+        Y[i+1]=Y[i] + coeff*xi_y + C*F(X[i],Y[i],Z[i])[1]
+        Z[i+1]=Z[i] + coeff*xi_z + C*F(X[i],Y[i],Z[i])[2]
+        if indicator_membrane(argument(X[i+1],Y[i+1],Z[i+1]))==1:
+            exit_x = X[i+1]
+            exit_y = Y[i+1]
+            exit_z = Z[i+1]
+            counter[3] += 1
+            i+=1
+            break
+        elif indicator_ahem(argument(X[i+1],Y[i+1],Z[i+1]))==1:
+            exit_x = X[i+1]
+            exit_y = Y[i+1]
+            exit_z = Z[i+1]
+            counter[0] += 1
+            i+=1
+            break
+        elif indicator_poretop(argument(X[i+1],Y[i+1],Z[i+1]))==1:
+            exit_x = X[i+1]
+            exit_y = Y[i+1]
+            exit_z = Z[i+1]
+            counter[2] += 1
+            i+=1
+            break
+        elif indicator_molecule(argument(X[i+1],Y[i+1],Z[i+1]))==1:
+            exit_x = X[i+1]
+            exit_y = Y[i+1]
+            exit_z = Z[i+1]
+            counter[1] += 1
+            i+=1
+            break
         i+=1
-        break
-    elif indicator_poretop(argument(X[i+1],Y[i+1],Z[i+1]))==1:
-        exit_x = X[i+1]
-        exit_y = Y[i+1]
-        exit_z = Z[i+1]
-        counter[2] += 1
-        i+=1
-        break
-    elif indicator_molecule(argument(X[i+1],Y[i+1],Z[i+1]))==1:
-        exit_x = X[i+1]
-        exit_y = Y[i+1]
-        exit_z = Z[i+1]
-        counter[1] += 1
-        i+=1
-        break
-    i+=1
-if i>steps-2:
-    counter[4] += 1
-print counter
+    if i>steps-2:
+        counter[4] += 1
+    else:
+        EXIT_X = np.append(EXIT_X, np.array([exit_x]))
+        EXIT_Y = np.append(EXIT_Y, np.array([exit_y]))
+        EXIT_Z = np.append(EXIT_Z, np.array([exit_z]))
 
+np.save('exit_x',EXIT_X)
+np.save('exit_y',EXIT_Y)
+np.save('exit_z',EXIT_Z)
 
-X=X[:i]
-Y=Y[:i]
-Z=Z[:i]
+# X=X[:i]
+# Y=Y[:i]
+# Z=Z[:i]
 
-np.save('X',X)
-np.save('Y',Y)
-np.save('Z',Z)
+# np.save('X',X)
+# np.save('Y',Y)
+# np.save('Z',Z)
 
-print 'number of steps:',i
-# print 'time [microsec]=',time*1e-3
 import plot
