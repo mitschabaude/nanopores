f59cae4a6d60fdea4789850fca7c6aa475ae64b0
Benjamin <e1226175@student.tuwien.ac.at>
2016-01-15 09:09:32 +0100

    minor

---
+++ random_walk_aHem/back.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv ../np/run.py .
+mv ../np/plot.py .
+mv ../np/aHem_array.py .
+mv ../np/calculateforce.py .
---
+++ random_walk_aHem/move.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv run.py ../np
+mv plot.py ../np
+mv aHem_array.py ../np
+mv calculateforce.py ../np
