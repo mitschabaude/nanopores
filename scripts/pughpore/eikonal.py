92226585cf5d4ab7d34ee19a5e438243f2127b7f
gregor <gregor.mitscha-baude@gmx.at>
2016-11-21 08:17:55 +0100

    minor

--- scripts/pughpore/eikonal.py
+++ scripts/pughpore/eikonal.py
@@ -10,16 +10,16 @@ up = nano.user_params(h=8.0)
 def distance_boundary():
     h = up.h
     geo = pughpore.get_geo(h)
-    
+
     mesh = geo.mesh
     V = FunctionSpace(mesh, "CG", 1)
     v = TestFunction(V)
     u = TrialFunction(V)
     f = Constant(1.0)
     y = Function(V)
-    
+
     bc = geo.BC(V, Constant(0.), "dnab").bcs
-    
+
     # Initialization problem to get good initial guess for nonlinear problem:
     F1 = inner(grad(u), grad(v))*dx - f*v*dx
     problem = LinearVariationalProblem(lhs(F1), rhs(F1), y, bc)
@@ -28,24 +28,24 @@ def distance_boundary():
     params["linear_solver"] = "cg"
     params["preconditioner"] = "hypre_euclid"
     solver.solve()
-    
+
     # Stabilized Eikonal equation
     print "max cell size:", mesh.hmax()
     eps = Constant(mesh.hmax()/25.)
     F = sqrt(inner(grad(y), grad(y)))*v*dx - f*v*dx + eps*inner(grad(y), grad(v))*dx
     # also works:
     #F = inner(grad(y), grad(y))*v*dx - f*v*dx + eps*inner(grad(y), grad(v))*dx
-    
+
     problem = NonlinearVariationalProblem(F, y, bc, J=derivative(F, y))
     solver  = NonlinearVariationalSolver(problem)
     params = solver.parameters
-    params["newton_solver"]["linear_solver"] = "bicgstab"
-    params["newton_solver"]["preconditioner"] = "hypre_euclid"
+    #params["newton_solver"]["linear_solver"] = "bicgstab"
+    #params["newton_solver"]["preconditioner"] = "hypre_euclid"
     solver.solve()
     #solve(F==0, y, bc, solver_parameters=params)
-    
+
     print "Max distance:", y.vector().max()
-    
+
     if not fields.exists("pugh_distance", h=h):
         fields.save_functions("pugh_distance", dict(h=h), y=y)
         fields.update()
