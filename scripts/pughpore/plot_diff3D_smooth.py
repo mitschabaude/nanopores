3e8c50aafbee0e34fbf1f611839bdf9200b19284
Benjamin <e1226175@student.tuwien.ac.at>
2017-01-26 14:05:08 +0100

    minor - load diffusivity in channel and smooth

--- scripts/pughpore/plot_diff3D_smooth.py
+++ scripts/pughpore/plot_diff3D_smooth.py
@@ -27,7 +27,15 @@ data = fields.get_fields("pugh_diff3D_cross", bulkbc=True, rMolecule=2.0779)
 ##plt.legend(bbox_to_anchor=(1.05, 1.), loc="upper left", borderaxespad=0.,)
 ##nanopores.savefigs("pugh_diff3D", folders.FIGDIR)
 #plt.show()
-def smooth(l):
+def smooth3(l):
+    A=np.array(l)
+    B=A[:]
+    ker=np.array([1./3,1./3,1./3])
+    n=int(ker.shape[0]/2.)
+    for i in range(n,A.shape[0]-n):
+        B[i]=np.inner(A[i-n:i+n+1],ker)
+    return list(B)
+def smooth5(l):
     A=np.array(l)
     B=A[:]
     ker=np.array([.2,.2,.2,.2,.2])
@@ -43,10 +51,18 @@ def smooth(l):
 x = [z[0] for z in data["x"]]
 data, x = fields._sorted(data, x)
 dstr = ["x", "y", "z"]
-for i in range(3):
-    Dxx = [D[i][i] for D in data["D"]]
-    style = "s-"
-    plt.plot(x, smooth(Dxx), style, label=r"$D_{%s%s}$" % (dstr[i], dstr[i]))
+x.append(1.0)
+
+Dxx = [D[0][0] for D in data["D"]]
+Dyy = [D[1][1] for D in data["D"]]
+Dzz = [D[2][2] for D in data["D"]]
+Dxx.append(0.)
+Dyy.append(0.)
+Dzz.append(0.)
+style = "s-"
+plt.plot(x, smooth5(Dxx), style, label=r"$D_{%s%s}$" % (dstr[0], dstr[0]))
+plt.plot(x, smooth3(Dyy), style, label=r"$D_{%s%s}$" % (dstr[1], dstr[1]))
+plt.plot(x, smooth3(Dzz), style, label=r"$D_{%s%s}$" % (dstr[2], dstr[2]))
 
 #plt.plot(x, [D2D]*len(x), "--k", label="2D cyl.")
 plt.xlabel("distance from pore center [nm]")
