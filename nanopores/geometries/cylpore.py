58cc59f509d7604117da01c676ca276ca3f73702
gregor <gregor.mitscha-eibl@gmx.at>
2017-02-23 15:59:27 +0100

    write operations on polygons needed for general framework

--- nanopores/geometries/cylpore.py
+++ nanopores/geometries/cylpore.py
@@ -1,5 +1,6 @@
 # (c) 2017 Gregor Mitscha-Baude
 import nanopores.py4gmsh as gmsh
+from nanopores.tools.polygons import PorePolygon
 from nanopores.tools.utilities import Params
 
 default = dict(
@@ -12,11 +13,9 @@ default = dict(
     lcCenter = 0.5,
     hmem = 2.2,
     zmem = 0.,
-    crosssections = (), # list of z coordinates
+    cs = (), # crosssections; list of z coordinates
     poreregion = False, # whether to include fluid above pore as subdomain
 )
-
-
 
 def get_geo(poly, h=1., **params):
     # get params
@@ -30,107 +29,27 @@ def get_geo(poly, h=1., **params):
     lcCenter = params.lcCenter
     hmem = params.hmem
     zmem = params.zmem
-    crosssections = sorted(params.crosssections)
+    cs = sorted(params.cs)
 
     # compute pore maximum and minimum z, length
     Z = [x[1] for x in poly]
     ztop = max(Z)
     zbot = min(Z)
-    hpore = ztop - zbot
-    assert all(zbot < z < ztop for z in crosssections)
 
-    #Anchor Points on aHem for membran (index)
-    ap1 = 18
-    ap2 = 49
-    apdiff=ap2-ap1
+    poly = PorePolygon(poly)
+    pmem = p.get_membrane(hmem, zmem, R)
+    pbot, pctr, ptop = p.get_poresections(cs)
 
-    #Anchor Points in aHem for CrossS (index)
-    ac1 = 52
-    ac2 = 68
-    ac3 = 82
-    ac4 = 0
-    zcross = sorted([X_aHem[i][2] for i in [ac1, ac2, ac3, ac4]])
-    params["lbtm"] = -zcross[0] + zcross[1]
-    params["lctr"] = -zcross[1] + zcross[2]
-    params["ltop"] = -zcross[2] + zcross[3]
-
-    params["zporetop"] = zcross[3]
-    params["zporebtm"] = zcross[0]
-    params["ztop"] = params["zporetop"] + l3
-    params["zbtm"] = params["zporebtm"] - l4
-
-    r0=max([X_aHem[index][0] for index in [ac1, ac2, ac3, ac4]])+rMolecule
-
-    X_Fluid_ext = numpy.array([[0.0, 0.0, l3],
-                               [R, 0.0, l3],
-                               [R, 0.0, X_aHem[ap1][2]],
-                               [R, 0.0, X_aHem[ap2][2]],
-                               [R, 0.0, -l0-l1-l4],
-                               [0.0, 0.0, -l0-l1-l4]])
-    X_Fluid_ctr = numpy.array([[0.0, 0.0, X_aHem[ac1][2]],
-                               [0.0, 0.0, X_aHem[ac2][2]],
-                               [0.0, 0.0, X_aHem[ac3][2]],
-                               [0.0, 0.0, X_aHem[ac4][2]]])
+    p.plot()
+    pmem.plot()
+    top, bottom = p.get_bulkfluids(R, H, p, pmem, pctr, pbot)
+    top.plot()
+    bottom.plot()
+    plt.show()
 
 
-    p_Fluid = [Point(x, lcOuter) for x in X_Fluid_ext]
-    p_Fluid.extend([Point(y, lcCenter) for y in X_Fluid_ctr])
-    p_aHem = [Point(x, lcCenter) for x in X_aHem]
-
-    #Create Line Loops from the points sitting on the line
-    Comment(' Connect all Fluid points ')
-    e_Fluid = [Line(p_Fluid[k], p_Fluid[k+1]) for k in range(len(p_Fluid)-1)]
-    e_Fluid.append(Line(p_Fluid[-1], p_Fluid[0]))
-
-    Comment(' Connect all aHem points ')
-    e_aHem = [Line(p_aHem[k], p_aHem[k+1]) for k in range(len(p_aHem)-1)]
-    e_aHem.append(Line(p_aHem[-1], p_aHem[0]))
-
-#    e_Membrane = [Line(p_aHem[ap1],p_Fluid[2]), Line(p_Fluid[3], p_aHem[ap2])]
-    e_Membrane = [Line(Point(X_aHem[ap1],lcMembrane), Point(numpy.array([R,0.,X_aHem[ap1][2]]),lcMembrane)),\
-                  Line(Point(numpy.array([R,0.,X_aHem[ap2][2]]),lcMembrane),Point(X_aHem[ap2],lcMembrane))]
-
-    edges_to_rot = [e_Fluid[0:5], e_aHem, e_Membrane]
-
-    geo_cs_str = "no crosssectional surface"
-    if crosssections:
-        Comment(' integrate crosssectional lines in fluid and check if molecule intersects lines')
-        e_CrossS = [Line(p_aHem[ac1], p_Fluid[6]), Line(p_aHem[ac2], p_Fluid[7]), Line(p_aHem[ac3], p_Fluid[8]), Line(p_aHem[ac4], p_Fluid[9])]
-        cs_pop_i = None
-        # check if molecule is near pore
-        if x0 is not None and (x0[0]**2 + x0[1]**2 <= r0**2):
-            # check z coordinate of molecule
-            if abs(x0[2] - X_aHem[ac4][2]) < rMolecule:
-                geo_cs_str = "top crosssection"
-                cs_pop_i = -1
-            elif abs(x0[2] - X_aHem[ac3][2]) < rMolecule:
-                geo_cs_str = "center top crosssection"
-                cs_pop_i = 2
-            elif abs(x0[2] - X_aHem[ac2][2]) < rMolecule:
-                geo_cs_str = "center bottom crosssection"
-                cs_pop_i = 1
-            elif abs(x0[2] - X_aHem[ac1][2]) < rMolecule:
-                geo_cs_str = "bottom crosssection"
-                cs_pop_i = 0
-        if cs_pop_i is not None:
-            e_CrossS.pop(cs_pop_i)
-            if cs_pop_i == 0:
-                top_acdiff = len(X_aHem)-ap1
-                bottom_end = ac2
-            elif cs_pop_i == 1:
-                top_acdiff = len(X_aHem)-ap1
-                bottom_end = ac3
-            elif cs_pop_i == 2:
-                top_acdiff = ac2-ap1
-                bottom_end = ac1
-            elif cs_pop_i == -1:
-                top_acdiff = ac3-ap1
-                bottom_end = ac1
-        edges_to_rot.append(e_CrossS)
-        top_acdiff = len(X_aHem)-ap1
-        bottom_end = ac1
 
 if __name__ == "__main__":
     from alphahempoly import poly
-    cross = [-0.5]
+    cross = [-3, -6]
     get_geo(poly, h=1., crosssections=cross)
--- nanopores/tools/polygons.py
+++ nanopores/tools/polygons.py
@@ -1,54 +1,220 @@
 # (c) 2017 Gregor Mitscha-Baude
 """this was intended to perform boolean operations on polygons,
 but this seems to be too hard without relying on external software"""
+from matplotlib import pyplot as plt
+#from nanopores.tools.utilities import union, collect
 
-def clip(subjectPolygon, clipPolygon):
-   def inside(p):
-      return(cp2[0]-cp1[0])*(p[1]-cp1[1]) > (cp2[1]-cp1[1])*(p[0]-cp1[0])
+class Polygon(object):
+    TOL = 0.1
 
-   def computeIntersection():
-      dc = [ cp1[0] - cp2[0], cp1[1] - cp2[1] ]
-      dp = [ s[0] - e[0], s[1] - e[1] ]
-      n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
-      n2 = s[0] * e[1] - s[1] * e[0]
-      n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
-      return [(n1*dp[0] - n2*dc[0]) * n3, (n1*dp[1] - n2*dc[1]) * n3]
+    def __init__(self, nodes):
+        self.nodes = [(float(x[0]), float(x[1])) for x in nodes]
+        self.init_edges()
 
-   outputList = subjectPolygon
-   cp1 = clipPolygon[-1]
+    def init_edges(self):
+        nodes = self.nodes
+        self.edges = zip(nodes, nodes[1:] + nodes[0:1])
 
-   for clipVertex in clipPolygon:
-      cp2 = clipVertex
-      inputList = outputList
-      outputList = []
-      s = inputList[-1]
+    def intersections(self, z):
+        z = float(z)
+        return {v: vnodes for edge in self.edges \
+                          for v, vnodes in self.intersect_edge(edge, z)}
 
-      for subjectVertex in inputList:
-         e = subjectVertex
-         if inside(e):
-            if not inside(s):
-               outputList.append(computeIntersection())
-            outputList.append(e)
-         elif inside(s):
-            outputList.append(computeIntersection())
-         s = e
-      cp1 = cp2
-   return(outputList)
+    def left_intersection(self, z):
+        I = self.intersections(z)
+        if not I: return
+        x = min(I.keys(), key=lambda t: t[0])
+        self.add(x, I[x])
+        return x
 
-def horizontal_intersect(poly, a, b):
-    poly2 = [[-100, a], [-100, b], [100, b], [100, a]]
-    return clip(poly2, poly)
+    def right_intersection(self, z):
+        I = self.intersections(z)
+        if not I: return
+        x = max(I.keys(), key=lambda t: t[0])
+        self.add(x, I[x])
+        return x
+
+    def intersect_edge(self, edge, z):
+        # return intersection points with line z=z
+        # and vertices to provide context for insertion
+        x, y = edge
+        x0, x1 = x
+        y0, y1 = y
+        # special case
+        if x1 == y1 == z:
+            return {(x, ()), (y, ())}
+        elif x1 <= z < y1 or y1 < z <= x1:
+            # special case
+            if abs(y1 - x1) < 1e-3*self.TOL:
+                return {(x, ()), (y, ())}
+            # x1 + (y1 - x1)*t == z
+            # at this point uniquely solvable in (0,1)
+            t = (z - x1)/(y1 - x1)
+            v = (x0 + (y0 - x0)*t, x1 + (y1 - x1)*t)
+            if self.close(x, v):
+                vnodes = () if x == v else (x,)
+            elif self.close(y, v):
+                vnodes = () if y == v else (y,)
+            else:
+                vnodes = (x, y)
+            return {(v, vnodes)}
+        else:
+            return set()
+
+    def add(self, v, context):
+        if len(context) == 0:
+            # v is already node, we have to do nothing!"o-k"
+            return
+        elif len(context) == 1:
+            # replace a node with v
+            x, = context
+            i = self.nodes.index(x)
+            self.nodes[i] = v
+        elif len(context) == 2:
+            # insert between two nodes
+            x, y = context
+            i = self.nodes.index(y)
+            self.nodes.insert(i, v)
+        self.init_edges()
+
+    def close(self, x, y):
+        return sum((t - s)**2 for t, s in zip(x, y)) < self.TOL**2
+
+    def plot(self, *args, **kwargs):
+        #plt.figure()
+        x0, x1 = zip(*(self.nodes + [self.nodes[0]]))
+        plt.plot(x0, x1, *args, **kwargs)
+
+    def clip_from_right(self, a1, b1, c0):
+        # return polygon a-b-c-d which is bounded at the left a-b side by self
+        # and add intersection nodes to self
+        a = self.right_intersection(a1)
+        b = self.right_intersection(b1)
+        c = (c0, b1)
+        d = (c0, a1)
+        nodes = self.nrange(a, b, -1) + [c, d]
+        return Polygon(nodes)
+
+    def clip_from_left(self, d1, c1, a0):
+        # return polygon a-b-c-d which is bounded at the right c-d side by self
+        # and add intersection nodes to self
+        c = self.left_intersection(c1)
+        d = self.left_intersection(d1)
+        a = (a0, d1)
+        b = (a0, c1)
+        nodes = [a, b] + self.nrange(c, d, -1)
+        return Polygon(nodes)
+
+    def nrange(self, a, b, sign=1):
+        # return nodes that range from a to b (including b)
+        # sign = 1 for counter-clockwise, -1 for clockwise
+        n = self.len()
+        ia = self.index(a)
+        ib = self.index(b)
+        if sign == -1:
+            ib = (ib - 1) % n
+            if ib > ia:
+                ia += n
+            ran = range(ia, ib, -1)
+        elif sign == 1:
+            ib = (ib + 1) % n
+            if ia > ib:
+                ib += n
+            ran = range(ia, ib)
+        else:
+            raise NotImplementedError
+        return [self.nodes[i % n] for i in ran]
+
+    def index(self, x):
+        return self.nodes.index(x)
+
+    def len(self):
+        return len(self.nodes)
+
+class PorePolygon(Polygon):
+    def get_membrane(self, hmem, zmem, R):
+        zbot = zmem - 0.5*hmem
+        ztop = zmem + 0.5*hmem
+        return self.clip_from_right(zbot, ztop, R)
+
+    def get_poresections(self, cs=None):
+        # cs ... crosssections
+        ztop = max(x[1] for x in self.nodes)
+        zbot = min(x[1] for x in self.nodes)
+        cs = [] if cs is None else list(cs)
+        assert all(zbot < z < ztop for z in cs)
+        cs = [zbot] + sorted(cs) + [ztop]
+        pairs = zip(cs[:-1], cs[1:])
+        return tuple([self.clip_from_left(a, b, 0) for a, b in pairs])
+
+    def get_bulkfluids(self, R, H, *polygons):
+        upper = compute_upper_boundary(*polygons)
+        lower = compute_lower_boundary(*polygons)
+        nodes = [(0, H/2.), (R, H/2.)] + upper
+        bulkfluid_top = Polygon(nodes)
+        nodes = lower + [(R, -H/2.), (0, -H/2.)]
+        bulkfluid_bottom = Polygon(nodes)
+        return bulkfluid_top, bulkfluid_bottom
+
+
+def join_nodes(polygons):
+    return list(set([x for p in polygons for x in p.nodes]))
+
+def join_edges(polygons):
+    return list(set([x for p in polygons for x in p.edges]))
+
+def top_edge_from_node(node, edges):
+    edges = [e for e in edges if e[0]==node]
+    assert len(edges) > 0
+    return max(edges, key=lambda e: e[1][1])
+
+def bottom_edge_from_node(node, edges):
+    edges = [e for e in edges if e[1]==node]
+    assert len(edges) > 0
+    return min(edges, key=lambda e: e[0][1])
+
+def compute_upper_boundary(*polygons):
+    allnodes = join_nodes(polygons)
+    edges = join_edges(polygons)
+    # start at top node with r=0, go until rmax
+    x0 = max([x for x in allnodes if x[0]==0], key=lambda x: x[1])
+    rmax = max([x[0] for x in allnodes])
+    X = [x0]
+    while x0[0] < rmax:
+        edge = top_edge_from_node(x0, edges)
+        x0 = edge[1]
+        X.append(x0)
+    return X[::-1]
+
+def compute_lower_boundary(*polygons):
+    allnodes = join_nodes(polygons)
+    edges = join_edges(polygons)
+    # start at bottom node with r=0, go until rmax
+    x0 = min([x for x in allnodes if x[0]==0], key=lambda x: x[1])
+    rmax = max([x[0] for x in allnodes])
+    X = [x0]
+    while x0[0] < rmax:
+        edge = bottom_edge_from_node(x0, edges)
+        x0 = edge[0]
+        X.append(x0)
+    return X
 
 if __name__ == "__main__":
-    poly1 = [[0,0], [2,0], [2,2], [0,2]]
-    poly2 = [[1,1], [3,1], [3,3], [1,3]]
-    poly3 = [[0.5,-1], [0.5,3], [1.5,3], [1.5,-1]]
-    u = [[0,-2], [0,2], [0.5,2], [0.5,-1], [1.5,-1], [1.5,2], [2,2], [2,-2]]
+    from nanopores.geometries.alphahempoly import poly
+    R = 10
+    H = 30
+    hmem = 2
+    zmem = -6
+    cs = [-2, -4]
 
-    #print "p1", poly1
-    #print "p2", poly2
-    #print "intersection", clip(poly1, poly2)
-    #
-    #print clip(u, poly1)
+    p = PorePolygon(poly)
+    pmem = p.get_membrane(hmem, zmem, R)
+    pbot, pctr, ptop = p.get_poresections(cs)
 
-    print horizontal_intersect(poly1, .5, 1.5)
+    p.plot()
+    pmem.plot()
+    top, bottom = p.get_bulkfluids(R, H, p, pmem, pctr, pbot)
+    top.plot()
+    bottom.plot()
+    plt.show()
+
