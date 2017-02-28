0736b234abbf802052c99e866b561b5a6ddd0255
Stadlbauer Benjamin <benjamin@l31.cm.cluster>
2017-02-28 12:23:23 +0100

    delete files

--- scripts/pughpore/randomwalk/attempt/init.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-file=open('taus','r')
-for line in file:
-    tau=line.split('\n')[0]
-    np.save('data2'+tau,np.array([]))
-
--- scripts/pughpore/randomwalk/attempt/prop.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-file=open('taus','r')
-for line in file:
-    tau=line.split('\n')[0]
-    data = np.load('data'+str(tau)+'.npy')
-    print tau
-    print np.mean(data)
