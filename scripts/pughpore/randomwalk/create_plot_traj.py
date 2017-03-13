01715d68b0e425a27b3be365992ea4b8913e9e57
Benjamin <e1226175@student.tuwien.ac.at>
2017-03-13 23:23:58 +0100

    sync

--- scripts/pughpore/randomwalk/create_plot_traj.py
+++ scripts/pughpore/randomwalk/create_plot_traj.py
@@ -1,5 +1,4 @@
 from matplotlib.ticker import FormatStrFormatter
-from matplotlib import gridspec
 #from matplotlib import rc
 #rc('text', usetex=True)
 import nanopores as nano
@@ -45,66 +44,56 @@ def save_fig(params,fieldsname,i):
     for i in range(1,T.shape[0]):
         T[i]=T[i]+T[i-1]
     tau_off=T[-1]
-    figname = fieldsname+'_traj_'+'%.3f'%(tau_off*1e-6)+'_%i'%i+'_%.1e_%.1e_%.1e_%.1e_%.1e_%.1e'%(params["avgbind1"],params["avgbind2"],params["avgbind3"],params["P_bind1"],params["P_bind2"],params["P_bind3"])+str(params["z0"])
-    #print 'tau_off = %.3f ms'% (tau_off*1e-6)
-    #print 'amplitude = %.0f pA'% amplitude
-    #J=np.append(np.array([curr,curr]),J)
-    #J=np.append(J,np.array([curr,curr]))
+    figname = fieldsname+'_traj_'+'%f'%(tau_off*1e-6)+'_%i'%i+'_%.1e_%.1e_%.1e_%.1e_%.1e_%.1e'%(params["avgbind1"],params["avgbind2"],params["avgbind3"],params["P_bind1"],params["P_bind2"],params["P_bind3"])+str(params["z0"])
     J=J*1e12
-    #T=np.append(np.array([-1e6,0.]),T)
-    #T=np.append(T,np.array([tau_off,1e6+tau_off]))
-    #T=T*1e-6
 
-    fig=plt.figure(figsize=(10,5),dpi=80)
-    gs=gridspec.GridSpec(1, 2, width_ratios=[1,1])
+    fig=plt.figure(figsize=(8,5),dpi=80)
 
-    ax0=plt.subplot(gs[0])
     plt.title('Trajectory')
-    ax0.plot(X,Z,linewidth=1.,c='#0000ff')
-    ax0.plot([l3/2.-.5,l3/2.-.5],[-3.,11.],color='#ff0000',linewidth=2.)
-    ax0.plot([-l3/2.+.5,-l3/2.+.5],[-3.,11.],color='#ff0000',linewidth=2.)
-#    ax0.plot([l1/2.,l1/2.],[17.,19.],color='#ffa500',linewidth=2.)
-#    ax0.plot([-l1/2.,-l1/2.],[17.,19.],color='#ffa500',linewidth=2.)
-#    ax0.plot([l3/2.,l3/2.,l2/2.,l2/2.],[-hpore/2.,hpore/2.-h2,hpore/2.-h2,14.],color='#ffa500',linewidth=2.)
-#    ax0.plot([-l3/2.,-l3/2.,-l2/2.,-l2/2.],[-hpore/2.,hpore/2.-h2,hpore/2.-h2,14.],color='#ffa500',linewidth=2.)
-    longer = ax0.scatter(X[bind1],Z[bind1],s=200,marker='h',c='#ff0000',linewidth=0.)
-    shorter = ax0.scatter(X[bind2],Z[bind2],s=100,marker='h',c='#ffa500',linewidth=0.)
-    start = ax0.scatter([X[0]],[Z[0]],s=200,marker='x',c='#00cc00',linewidth=2.)
-#    ax0.legend([start,longer,shorter],['Start','Long binding','Short binding'],scatterpoints=1,loc=(.03,.3))
-    ax0.legend([start,longer],['Start','Long binding'],scatterpoints=1,loc=(.03,.3))
+    plt.plot(X,Z,linewidth=1.,c='#0000ff')
+    plt.plot([l3/2.-.5,l3/2.-.5],[-3.,11.],color='#ff0000',linewidth=2.)
+    plt.plot([-l3/2.+.5,-l3/2.+.5],[-3.,11.],color='#ff0000',linewidth=2.)
+    plt.plot([l1/2.,l1/2.],[17.,19.],color='#ffa500',linewidth=2.)
+    plt.plot([-l1/2.,-l1/2.],[17.,19.],color='#ffa500',linewidth=2.)
+    plt.plot([l3/2.,l3/2.,l2/2.,l2/2.],[-hpore/2.,hpore/2.-h2,hpore/2.-h2,14.],color='#ffa500',linewidth=2.)
+    plt.plot([-l3/2.,-l3/2.,-l2/2.,-l2/2.],[-hpore/2.,hpore/2.-h2,hpore/2.-h2,14.],color='#ffa500',linewidth=2.)
+    longer = plt.scatter(X[bind1],Z[bind1],s=200,marker='h',c='#ff0000',linewidth=0.)
+    shorter = plt.scatter(X[bind2],Z[bind2],s=100,marker='h',c='#ffa500',linewidth=0.)
+    start = plt.scatter([X[0]],[Z[0]],s=200,marker='x',c='#00cc00',linewidth=2.)
+    plt.legend([start,longer,shorter],['Start','Long binding','Short binding'],scatterpoints=1,loc=(.42,.15))
     ax=plt.gca()
     ax.set_aspect('equal')
-    ax.set_xlim([-50.,20.])
-    ax.set_ylim([-30.,40.])
+    ax.set_xlim([20.,-55.])
+    ax.set_ylim([-25.,40.])
     ax.set_xticks([])
     ax.set_yticks([])
     plot_polygon(ax,polygon(rmem=60.))
 
 
-    ax1=plt.subplot(gs[1])
+    plt.axes([.55,.5,.2,.3])
     plt.title('Current signal')
     ax=plt.gca()
     if tau_off<1e3:
-        t = np.linspace(0.,tau_off,5)
+        t = np.linspace(0.,tau_off,3)
         fac=1.
-        ax.set_xlabel('Time [$ns$]')
+        ax.set_xlabel('time [$ns$]')
     elif tau_off<1e6 and tau_off>=1e3:
-        t = np.linspace(0.,tau_off*1e-3,5)
+        t = np.linspace(0.,tau_off*1e-3,3)
         fac = 1e-3
-        ax.set_xlabel(r'Time [$\mu s$]')
+        ax.set_xlabel(r'time [$\mu s$]')
     else:
-        t = np.linspace(0.,tau_off*1e-6,5)
+        t = np.linspace(0.,tau_off*1e-6,3)
         fac = 1e-6
-        ax.set_xlabel('Time [$ms$]')
+        ax.set_xlabel('time [$ms$]')
     T=T*fac
-    ax1.plot(T,J,linewidth=2.,color='#0000ff')
-    yt = np.linspace(580.,760,7)
-    ax.set_ylabel(r'Current [$pA$]')
+    plt.plot(T,J,color='#000000')
+    yt = np.linspace(580.,760,4)
+    ax.set_ylabel(r'A [$pA$]')
     ax.set_yticks(yt)
     ax.set_xticks(t)
     xfmt=FormatStrFormatter('%.1f')
     ax.xaxis.set_major_formatter(xfmt)
-    ax.set_xlim([-1e-2*tau_off*fac,(1.+1e-2)*tau_off*fac])
+    ax.set_xlim([-4e-2*tau_off*fac,(1.+4e-2)*tau_off*fac])
 
 
     plt.tight_layout()
--- scripts/pughpore/randomwalk/create_plot_type.py
+++ scripts/pughpore/randomwalk/create_plot_type.py
@@ -21,71 +21,39 @@ geop = nano.Params(pughpore.params)
 hpore=geop.hpore
 #params=dict(avgbind=8.7e6,P_bind=5.e-3,z0=hpore/2.+5.)
 def save_fig(params,fieldsname):
-	figname = fieldsname+'_%.1e_%.1e_%.1e_%.1e_%.1e_%.1e'%(params["avgbind1"],params["avgbind2"],params["avgbind3"],params["P_bind1"],params["P_bind2"],params["P_bind3"])+str(params["z0"])
-	data=f.get_fields(fieldsname,**params)
-	t1 = data["t1"]
-	a1 = data["a1"]
-	t2 = data["t2"]
-	a2 = data["a2"]
-	t0 = data["t0"]
-	a0 = data["a0"]
-	print len(t1)
-	print len(t2)
-	lendata=len(t1)+len(t2)
-	for i in range(len(t1)):
-		if math.isnan(a1[i]): print 'a1: NaN %i'%i
-#	print 't1'
-#	print t1
-#	print 'a1'
-#	print a1
-#	print 't2'
-#	print t2
-#	print 'a2'
-#	print a2
-#	t1 = np.array([])
-#	t2 = np.array([])
-#	a1 = np.array([])
-#	a2 = np.array([])
-#	print '#data = %i'%len(T)
-#	for i in range(len(T)):
-#	    T_=np.array(T[i])*1e-6
-#	    J_=np.array(J[i])
-#	    tau_off=np.sum(T_)
-#	    amp = (2060.-np.inner(J_,T_)/tau_off)/2060.*100
-#	    if number: ax.text(tau_off,amp,'%i'%i,fontsize=9)
-#	    if tau_off<.1:
-#		t1=np.append(t1,np.array([tau_off]))
-#		a1=np.append(a1,np.array([amp]))
-#	    else:
-#		t2=np.append(t2,np.array([tau_off]))
-#		a2=np.append(a2,np.array([amp]))
-
-
-
-	plt.figure(figsize=(6,4),dpi=80)
-	plt.plot([3e-6,.7],[0.,0.],linewidth=2,color='lightgreen')
-	plt.plot([1.,1e2],[0.,0.],linewidth=2,color='green')
-	ax=plt.gca()
-	plt.scatter(t1,a1,color='lightgreen',s=8)
-	plt.scatter(t2,a2,color='green',s=8)
-	plt.scatter(t0,a0,marker='o',s=50,facecolors='none',edgecolors='#ff0000')
-	if number:
-		for k in range(len(t1)):
-			ax.text(t1[k],a1[k],'%i'%k,fontsize=9)
-		for k in range(len(t1),len(t1)+len(t2)):
-			ax.text(t2[k-len(t1)],a2[k-len(t1)],'%i'%k,fontsize=9)
-	xfmt=FormatStrFormatter('%g')
-	ax.set_xlim([1e-6,500.])
-	ax.set_ylim([-2.,25.0])
-	ax.set_xscale('log')
-	ax.xaxis.set_major_formatter(xfmt)
-	ax.invert_yaxis()
-	ax.set_xlabel(r'$\tau_{off}$ [ms]',fontsize=15)
-	ax.set_ylabel(r'A/I$_0$ [%]',fontsize=15)
-	ax.text(.001,-0.03,'I',fontsize=15)
-	ax.text(5.,-0.03,'II',fontsize=15)
-#	ax.text(1.,5.,lendata,fontsize=25)
-	plt.tight_layout()
-	nano.savefigs(name=figname,DIR='/home/lv70496/benjamin/plots/')
-	print 'savefig:'
-	print figname
+    figname = fieldsname+'_%.1e_%.1e_%.1e_%.1e_%.1e_%.1e'%(params["avgbind1"],params["avgbind2"],params["avgbind3"],params["P_bind1"],params["P_bind2"],params["P_bind3"])+str(params["z0"])
+    data=f.get_fields(fieldsname,**params)
+    t1 = data["t1"]
+    a1 = data["a1"]
+    t2 = data["t2"]
+    a2 = data["a2"]
+    t0 = data["t0"]
+    a0 = data["a0"]
+    print len(t1)
+    print len(t2)
+    lendata=len(t1)+len(t2)
+    for i in range(len(t1)):
+            if math.isnan(a1[i]): print 'a1: NaN %i'%i
+    plt.figure(figsize=(6,4),dpi=80)
+    plt.plot([3e-6,.7],[0.,0.],linewidth=2,color='lightgreen')
+    plt.plot([1.,1e2],[0.,0.],linewidth=2,color='green')
+    ax=plt.gca()
+    plt.scatter(t1,a1,color='lightgreen',s=8)
+    plt.scatter(t2,a2,color='green',s=8)
+    plt.scatter(t0,a0,marker='o',s=50,facecolors='none',edgecolors='#ff0000')
+    #ax.text(t1[k],a1[k],'%i'%k,fontsize=9)
+    xfmt=FormatStrFormatter('%g')
+    ax.set_xlim([1e-6,500.])
+    ax.set_ylim([-2.,25.0])
+    ax.set_xscale('log')
+    ax.xaxis.set_major_formatter(xfmt)
+    ax.invert_yaxis()
+    ax.set_xlabel(r'$\tau_{off}$ [ms]',fontsize=15)
+    ax.set_ylabel(r'A/I$_0$ [%]',fontsize=15)
+    ax.text(.001,-0.03,'I',fontsize=15)
+    ax.text(5.,-0.03,'II',fontsize=15)
+    #ax.text(1.,5.,lendata,fontsize=25)
+    plt.tight_layout()
+    nano.savefigs(name=figname,DIR='/home/lv70496/benjamin/plots/')
+    print 'savefig:'
+    print figname
