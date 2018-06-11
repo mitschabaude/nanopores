a9ecdac12b0beecdcb6e27bfb328f75fb17373f6
Benjamin Stadlbauer <e1226175@student.tuwien.ac.at>
2018-06-11 13:11:49 +0200

    newfigureforpaper

--- scripts/pughpore/randomwalk/create_plot_traj_without_dolfin.py
+++ scripts/pughpore/randomwalk/create_plot_traj_without_dolfin.py
@@ -1,22 +1,89 @@
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib
-import nanopores as nano
-import nanopores.geometries.pughpore as pughpore
-from nanopores.models.pughpore import polygon
-from nanopores.models.pughpoints import plot_polygon
+#import nanopores as nano
+#import nanopores.geometries.pughpore as pughpore
+#from nanopores.models.pughpore import polygon
+#from nanopores.models.pughpoints import plot_polygon
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import os
-import nanopores.tools.fields as f
-HOME = os.path.expanduser("~")
-PAPERDIR = os.path.join(HOME, "papers", "paper-howorka")
-FIGDIR = os.path.join(PAPERDIR, "figures", "")
-DATADIR = os.path.join(HOME,"Dropbox", "nanopores", "fields")
-f.set_dir(DATADIR)
+#import nanopores.tools.fields as f
+#HOME = os.path.expanduser("~")
+#PAPERDIR = os.path.join(HOME, "papers", "paper-howorka")
+#FIGDIR = os.path.join(PAPERDIR, "figures", "")
+#DATADIR = os.path.join(HOME,"Dropbox", "nanopores", "fields")
+#f.set_dir(DATADIR)
+###############################
+# (c) 2017 Gregor Mitscha-Baude
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-up = nano.Params(pughpore.params, k=3)
+default = dict(
+    R = 20.,
+    H = 70.,
+    l0 = 18., #22.5,
+    l1 = 14., #17.5,
+    l2 = 10., #12.5,
+    l3 = 6., #7.5,
+    l4 = 14., #17.5,
+    hpore = 46,
+    hmem = 2.2,
+
+    h2 = 46.-35., # 11.
+    h1 = 46.-35.-2.5, # 8.5
+    h4 = 10.,
+
+    hnear = 12,
+
+    rMolecule = 2.0779, # molecular radius of protein trypsin
+    x0 = [0., 0., 0.],
+    lcMolecule = 0.4, # relative to global mesh size
+    center_at_x0 = False,
+    center_z_at_x0 = False,
+)
+
+class Params(dict):
+    "for writing params.Qmol instead of params['Qmol']"
+    def __getattr__(self, key):
+        return self[key]
+    def __or__(self, other):
+        new = Params(self)
+        new.update(other)
+        return new
+
+def plot_polygon(ax, polygon):
+    settings = dict(closed=True, facecolor="#eeeeee", linewidth=1.,
+                    edgecolor="black")
+    polygon = np.array(polygon)
+    polygon_m = np.column_stack([-polygon[:,0], polygon[:,1]])
+
+    patch = patches.Polygon(polygon, **settings)
+    patchm = patches.Polygon(polygon_m, **settings)
+    #patch.set_zorder(10)
+    #patchm.set_zorder(10)
+    ax.add_patch(patch)
+    ax.add_patch(patchm)
+
+def polygon(rmem=20., **params):
+    "polygon of pore + membrane for plotting"
+    #setup = SetupNoGeo(**params)
+    #params = nano.Params(pughpore.params) | setup.geop
+    params = Params(default, **params)
+
+    r = [0.5*params.l3, 0.5*params.l2, 0.5*params.l1, 0.5*params.l0,
+         0.5*params.l4, rmem]
+    ztop = params.hpore/2.
+    zbot = -ztop
+    z = [zbot, ztop - params.h2, ztop - params.h1, ztop, zbot + params.h4,
+         zbot + params.hmem]
+    # indices: [(0,0), (0,1), (1,1), (1,2), ..., (5,5), (5,0)]
+    return [(r[i / 2 % 6], z[(i+1) / 2 % 6]) for i in range(12)]
+###############################
+
+up = Params(default, k=3)
 hpore=up.hpore
 l0 =        up.l0
 l1 =        up.l1
@@ -34,11 +101,36 @@ h4 =        up.h4
 #showtraj = True
 
 #def save_fig_traj(params,fieldsname,i,showtraj):
+def assertdir(DIR):
+    if not os.path.exists(DIR):
+        os.makedirs(DIR)
+
+def savefigs(name="fig", DIR="/tmp/", size=None, pdf=False):
+    if not DIR.endswith("/"): DIR = DIR + "/"
+    assertdir(DIR)
+    if len(plt.get_fignums()) == 1:
+        fig = plt.figure(plt.get_fignums()[0])
+        if size is not None:
+            fig.set_size_inches(size)
+        if not pdf: suffix='.eps'
+        else: suffix='.pdf'
+        fig.savefig(DIR + name + suffix, bbox_inches="tight")
+        return
+    for num in plt.get_fignums():
+        fig = plt.figure(num)
+        label = fig.get_label()
+        label = str(num) if label=="" else label
+        if size is not None:
+            fig.set_size_inches(size)
+        if not pdf: suffix='.eps'
+        else: suffix='.pdf'
+        fig.savefig(DIR + name + "_" + label + suffix, bbox_inches="tight")
 def save_fig_traj():
-    showtraj = True
+    showtraj = False
 #    data=f.get_fields(fieldsname,**params)
     b1 = [[[l3/2.,-hpore/2.],[l3/2.,hpore/2.-h2],[l2/2.,hpore/2.-h2],[l2/2.,hpore/2.-h1],[l1/2.,hpore/2.-h1],[l1/2.,hpore/2.]]]
-    b2 = [[[l3/2.-.5,11.],[l3/2.-.5,-3.]]]
+#    b2 = [[[l3/2.-.5,11.],[l3/2.-.5,-3.]]]
+#    b2 = [[[l3/2.,-11.],[l3/2.,3.]]]
    # b1 =data["b1"]
    # b2 =data["b2"]
     if showtraj:
@@ -60,7 +152,7 @@ def save_fig_traj():
         figname = fieldsname+'_traj_'+'%.8f'%(tau_off*1e-6)+'_%04d'%i+'_%.1e_%.1e_%.1e_%.1e'%(params["avgbind1"],params["avgbind2"],params["P_bind1"],params["P_bind2"])+str(params["z0"])
     else:
 #        figname = fieldsname+'_bindzones'+'_%.1e_%.1e_%.1e_%.1e'%(params["avgbind1"],params["avgbind2"],params["P_bind1"],params["P_bind2"])+str(params["z0"])
-        figname = 'newfigurenew.eps'
+        figname = 'bindzones_both.eps'
 
     if showtraj:
         fig=plt.figure(figsize=(8,5),dpi=80)
@@ -77,7 +169,7 @@ def save_fig_traj():
         y= [p[1] for p in seq]
         plt.plot(x,y,color=color1,linewidth=2.)
         plt.plot(xm,y,color=color1,linewidth=2.)
-    #b2 = [[[l3/2.-.5,-3.],[l3/2.-.5,11.]]]
+    b2 = [[[l3/2.-.5,-4.],[l3/2.-.5,4.]]]
     for seq in b2:
         x= [p[0] for p in seq]
         xm=[-p[0] for p in seq]
@@ -141,7 +233,9 @@ def save_fig_traj():
 
 
     plt.tight_layout()
-#    nano.savefigs(name=figname,DIR='/home/bstadlbau/plots/')
-    plt.show()
+    savefigs(name=figname,DIR='/home/bstadlbau/plots/')
+#    plt.show()
 #    print 'savefig: %s'%figname
-#    plt.close("all")
+    plt.close("all")
+
+save_fig_traj()
